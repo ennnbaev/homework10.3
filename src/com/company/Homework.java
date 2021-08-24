689589bc44fff796894315d128f39d05ef7437f9
Danil IenbaIev <ennnbaev@icloud.com>
2021-08-24 11:36:51 +0300

    Homework done

--- src/com/company/Homework.java
+++ src/com/company/Homework.java
@@ -9,9 +9,9 @@ public class Homework {
     public static void main(String[] args) {
         List<Employee> collection=new ArrayList<>();
         collection.add(new Employee("Danil","Enbaev",55000));
-        collection.add(new Employee("Ann","Green",18000));
+        collection.add(new Employee("Jack","Green",18000));
         collection.add(new Employee("Denis","Maksymenko",18000));
-        collection.add(new Employee("Ann","White",18000));
+        collection.add(new Employee("Ann","Maksymenko",18000));
         System.out.println(collection);
        EmployeeComparator comparator=new EmployeeComparator();
         Collections.sort(collection,comparator);
